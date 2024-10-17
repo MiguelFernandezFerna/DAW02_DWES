078f9a878e2ebdc2ec959d449600fd358aba05ad
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-17 16:30:53 +0200

    Cani funcional

--- U2/234cani.php
+++ U2/234cani.php
@@ -1,22 +1,24 @@
 <?php
-    //EsCrIbE uNa FuNcIóN qUe TrAnSfOrMe UnA cAdEnA eN cAnI.
-    $cadena = "Soy Jose Luis Torrente";
-    function cani($cad){
-        $cadenaFinal = "";
-        $letra = "";
-        $tokens = strtok($cad," ");
-    while ($tokens!=false) {
-            $letra=$tokens;
-            for ($i=0; $i < strlen($letra); $i++) { 
-                if ($i%2==0) {
-                    $letra = strtoupper($letra);
+    $input = "Si el muxaxo te basila tu t kaya i lo asimila";
+    
+    
+    function convierteCani ($frase) {
+        $fraseCani = "";
+        $alternar = true;
+        for ($i = 0; $i<strlen($frase); $i++) {
+            if ($frase[$i]=== " ") {
+                $fraseCani.= " ";
+            } else {
+                if ($alternar) {
+                    $frase[$i] = strtoupper($frase[$i]); 
+                } else {
+                    $frase[$i] = strtolower($frase[$i]);
                 }
-                $cadenaFinal = $cadenaFinal.$letra." ";
-            } 
-    }
-            $tokens = strtok(" ");
-        
-        return $cadenaFinal;
+                $alternar = $alternar ? false : true; 
+            }
         }
-
-    echo cani($cadena);
+        return $frase;
+    }
+    echo convierteCani($input);
+    
+    ?>
