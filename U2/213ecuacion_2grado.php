275d02a21e6c632e7910ba5797367cb79ac3dbb4
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-21 20:35:23 +0200

    rehecho

--- U2/213ecuacion_2grado.php
+++ U2/213ecuacion_2grado.php
@@ -10,21 +10,23 @@
     <h1>Ejercicio 213</h1>
     <p>crea un programa que resuelva una ecuación de 2º grado del tipo ax² + bx + c = 0. Ten en cuenta que puede tener 2, 1 o no tener solución dependiendo del valor del discriminante b²-4ac. Para calcular la raíz cuadrada deberás utilizar la función sqrt().</p>
     <?php
-        $a=1;
-        $b=1;
-        $c=0;
-        $discriminante = ($b*$b)-4*$a*$c;
-        if ($discriminante=0) {
-            $result0=(-$b+sqrt($discriminante))/2*$a;
-            echo "La solución es: $result0";
-        } else if($discriminante<0){
-            echo "No hay solución";
-        }else{
-        $result1 = -$b+sqrt($discriminante)/(2*$a);
-        echo "El primer resultado es: $result1 <br>";
-        $result2 =-$b+sqrt($discriminante)/(2*$a);
-        echo "El segundo resultado es: $result2";
-        }
+          $a = 3;
+          $b = 6;
+          $c = 2;
+  
+          $ecuacion;
+          $discr = $b**2 - 4*$a*$c;
+          if ($discr > 0 ) {
+              $ecuacion = (-$b+sqrt($b**2-(4*$a*$c)))/(2*$a);
+              echo "Primera solución: $ecuacion <br>";
+              $ecuacion = (-$b-sqrt($b**2-(4*$a*$c)))/(2*$a);
+              echo "Segunda solución: $ecuacion <br>";
+  
+          } else if ($discr === 0) {
+              $ecuacion = (-$b)/2*$a;
+          } else {
+              echo "La ecuación no tiene solución, el discriminante es negativo";
+          }
     ?>
 </body>
 </html>
