ccd4d12bae4c283052c19cfa62c306ef68ec6024
MiguelFernandezFerna <167419837+MiguelFernandezFerna@users.noreply.github.com>
2024-10-23 14:10:36 +0200

    Add files via upload

---
+++ U3/301datosPersonales.php
@@ -0,0 +1,26 @@
+<?php
+    echo "Nombre: " . $_POST['nombre']. "<br>";
+    echo "Primer apellido: ". $_POST['apellido1']. "<br>";
+    echo "Segundo apellido: ". $_POST['apellido2']. "<br>";
+    echo "DNI: ". $_POST['dni']. "<br>";
+    echo "Email: ". $_POST['email']. "<br>";
+    echo "Fecha de nacimiento: ". $_POST['fecha']. "<br>";
+    echo "Teléfono: ". $_POST['telefono']. "<br>";
+    echo "Sexo: ". $_POST['sexo']. "<br>";
+    echo "Estudios: ". $_POST['estudios']. "<br>";
+    echo "Idiomas: ". $_POST['idiomas']. "<br>";
+    echo "Foto: ". $_FILES["foto"]["name"]. "<br>";
+    echo "Curriculum: " . $_FILES["curriculum"]["name"];
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
