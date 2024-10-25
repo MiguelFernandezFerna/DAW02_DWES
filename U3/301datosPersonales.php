125fb5ead17d94ee9c83854c61e686a9332eeaef
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-10-25 18:52:00 +0200

    301 completo y funcional

--- U3/301datosPersonales.php
+++ U3/301datosPersonales.php
@@ -1,16 +1,47 @@
 <?php
-    echo "Nombre: " . $_POST['nombre']. "<br>";
-    echo "Primer apellido: ". $_POST['apellido1']. "<br>";
-    echo "Segundo apellido: ". $_POST['apellido2']. "<br>";
-    echo "DNI: ". $_POST['dni']. "<br>";
-    echo "Email: ". $_POST['email']. "<br>";
-    echo "Fecha de nacimiento: ". $_POST['fecha']. "<br>";
-    echo "Teléfono: ". $_POST['telefono']. "<br>";
-    echo "Sexo: ". $_POST['sexo']. "<br>";
-    echo "Estudios: ". $_POST['estudios']. "<br>";
-    echo "Idiomas: ". $_POST['idiomas']. "<br>";
-    echo "Foto: ". $_FILES["foto"]["name"]. "<br>";
-    echo "Curriculum: " . $_FILES["curriculum"]["name"];
+    function filtrado($datos){
+        $datos = trim($datos); // Elimina espacios antes y después de los datos
+        $datos = stripslashes($datos); // Elimina backslashes \
+        $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
+        return $datos;
+    }
+
+    $directorioSubida = "subidos/";
+
+    if(isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
+        $nombre = filtrado($_POST["nombre"]);
+        $apellido1 = filtrado($_POST["apellido1"]);
+        $apellido2 = filtrado($_POST["apellido2"]);
+        $dni = filtrado($_POST["dni"]);
+        $email = filtrado($_POST["email"]);
+        $fechaNac = filtrado($_POST["fecha"]);
+        $telefono = filtrado($_POST["telefono"]);
+        $sexo = filtrado($_POST["sexo"]);
+        $estudios = filtrado($_POST["estudios"]);
+        // Utilizamos implode para pasar el array a string
+        $idiomas = filtrado(implode(", ", $_POST["idiomas"]));
+        $nombreArchivo = $_FILES['foto']['name'];
+        $nombreCompleto = $directorioSubida.$nombreArchivo;
+        $nombreFormulario = $_FILES['curriculum']['name'];
+        $nombreCompletoForm = $directorioSubida.$nombreFormulario;
+    }
+
+    echo "Nombre: $nombre <br>";
+    echo "Primer apellido: $apellido1 <br>";
+    echo "Segundo apellido: $apellido2 <br>";
+    echo "DNI: $dni <br>";
+    echo "Email: $email <br>";
+    echo "Fecha de nacimiento: $fechaNac <br>";
+    echo "Teléfono: $telefono <br>";
+    echo "Sexo: $sexo <br>";
+    echo "Estudios: $estudios <br>";
+    echo "Idiomas: $idiomas <br>";
+    echo "Imagen: <br><br>";
+    echo "<img src=".$nombreCompleto."> <br><br>";
+    echo "Formulario: <br><br>";
+    echo " <iframe src=".$nombreCompletoForm.">Descargar</iframe>";
+
+
     
 
     
