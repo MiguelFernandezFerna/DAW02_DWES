70431a0a83d825219dfd5add2bd3d26533924467
MIGUEL FERNÁNDEZ FERNÁNDEZ <miguel.ferfer@educa.jcyl.es>
2024-11-11 12:05:35 +0100

    Todo, no se porque no funciona

--- U3/Base de datos/insertar.php
+++ U3/Base de datos/insertar.php
@@ -1,23 +1,22 @@
 <?php
-    include("conexion.php");
+include("conexion.php");
 
-    $conexion = conectar();
-    $nombre = "Miguel";//$_POST["nombre"];
-    $apellidos = "kdfjsk";//$_POST["apellidos"];
-    $telefono = 666999888;//$_POST["telefono"];
+$conexion = conectar();
+$nombre = "Miguel";//$_POST["nombre"];
+$apellidos = "Fernandez";//$_POST["apellidos"];
+$telefono = 666999888;//$_POST["telefono"];
 
-    $consultaInsert=$conexion->prepare("insert into persona(nombre, apellidos, telefono) value(?,?,?)");
+$consultaInsert=$conexion->prepare("insert into persona(nombre, apellidos, telefono) value(?,?,?)");
 
-    $consultaInsert->bind_param("ssi", $nombre, $apellidos, $telefono);
+$consultaInsert->bind_param("ssi", $nombre, $apellidos, $telefono);
 
-    if ($consultaInsert->execute()) {
-        echo "Datos insertados: <br>";
-        echo $nombre."<br>";
-        echo $apellidos."<br>";
-        echo $telefono."<br>";
-    } else {
-        echo "Error, no se ha podido insertar";
-    }
+if ($consultaInsert->execute()) {
+    echo "Datos insertados: <br>";
+    echo $nombre."<br>";
+    echo $apellidos."<br>";
+    echo $telefono."<br>";
+} else {
+    echo "Error, no se ha podido insertar";
+}
 
-    $conexion->close();
-    
+$conexion->close();
