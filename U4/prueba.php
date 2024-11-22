45916a7b84a31f696a080b2812af07a08fe4eeb3
MIGUEL FERNÁNDEZ FERNÁNDEZ <miguel.ferfer@educa.jcyl.es>
2024-11-22 09:05:03 +0100

    prueba

---
+++ U4/prueba.php
@@ -0,0 +1,21 @@
+<?php
+//Primer ejemplo
+// $nombreArchivo = 'hola.txt';
+// $archivo = fopen($nombreArchivo, 'r');
+// if ($archivo) {
+// echo "El archivo se abrió correctamente.";
+//  fclose($archivo); // No olvides cerrar
+// } else {
+// echo "No se pudo abrir '$nombreArchivo'.";
+// }
+//Segundo ejemplo
+$fp = fopen("hola.txt", "r");
+if ($fp === FALSE) {
+echo "No existe el fichero o no se pudo leer<br>";
+}else{
+while(!feof($fp) ){
+$car=fgetc($fp);
+echo $car;
+}
+}
+fclose($fp);
