bed0bab68b184c60b2bc65b690562e13a1373343
Miguel Fernandez <miguel.ferfer@educa.jcyl.es>
2024-12-02 17:15:37 +0100

    Cambios

--- U3/Base de datos/pagina2/vista/visualizarProyectos.php
+++ U3/Base de datos/pagina2/vista/visualizarProyectos.php
@@ -4,7 +4,7 @@
     function visualizarDatos(){
         $conectar = conexion();
 
-        $consulta = "select * from proyecto";
+        $consulta = "select p.*, a.nombre as nomAlum from proyecto p left outer join alumnos a on p.alumno=a.id_alumno";
         $sentencia = $conectar -> prepare($consulta);
         $sentencia -> setFetchMode(PDO::FETCH_ASSOC);
         $sentencia -> execute();
@@ -25,7 +25,7 @@
             echo "<td>$proyecto[modulo1]</td>";
             echo "<td>$proyecto[modulo2]</td>";
             echo "<td>$proyecto[modulo3]</td>";
-            echo "<td>$proyecto[alumno]</td>";
+            echo "<td>$proyecto[nomAlum]</td>";
             echo "<td><button><a href='formulario_modificar_proyecto.php?id_proyecto=$proyecto[id_proyecto]'>Modificar</a></button></td>";
             echo "<td><button><a href='../controlador/eliminar.php?id_proyecto=$proyecto[id_proyecto]'>Eliminar</a></button></td>";
             echo "</tr>";
