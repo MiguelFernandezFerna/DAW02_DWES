2ac6dcf4a7b769731ae72915edb5d09448b6c469
MIGUEL FERNÁNDEZ FERNÁNDEZ <miguel.ferfer@educa.jcyl.es>
2025-01-20 14:09:47 +0100

    Cosas

--- U5/carritoCompra/vista/productos.php
+++ U5/carritoCompra/vista/productos.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../controlador/controlador.php';
+require_once '../controlador/controlador.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +11,15 @@
 <body>
     <h1>Productos: </h1>
     <ul>
-        <?php foreach ($productos as $indice => $producto) {?>
+        <?php foreach ($productos as $indice => $producto) { ?>
             <li>
-                <?= $producto->getNombre()?> - <?= $producto->getPrecio()?>&euro;
+                <?= $producto->getNombre() ?> - <?= $producto->getPrecio() ?>&euro;
                 <form action="../controlador/controlador.php" method="post">
-                    <input type="hidden" name="indice" value="<?= $indice?>">
+                    <input type="hidden" name="indice" value="<?= $indice ?>">
                     <input type="submit" value="Agregar al carrito" name="agregar">
                 </form>
             </li>
-        <?php }?>
+        <?php } ?>
     </ul>
     <p><a href="carrito.php">Ver carrito</a></p>
 </body>
