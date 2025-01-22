f600d59d94059f95367e0d7f3992f0bfae9d6597
MIGUEL FERNÁNDEZ FERNÁNDEZ <miguel.ferfer@educa.jcyl.es>
2025-01-22 14:08:20 +0100

    Cositas

---
+++ U6/equipo-jugador/jugador_equipo.php
@@ -0,0 +1,24 @@
+<?php
+  require_once "bootstrap.php";
+  require_once './src/Entity/Equipo.php';
+  require_once './src/Entity/Jugador.php';
+  //$id = $_GET['id'];
+  /*buscar el jugador con el id indicado*/
+  $jugador = $entityManager->find("Jugador", 2);
+  if(!$jugador){
+    echo "Jugador no encontrado";
+  }else{
+    echo "Nombre del jugador: ". $jugador->getNombre()."<br>";
+    echo "Apellidos del jugador: ". $jugador->getApellidos()."<br>";
+    echo "Edad del jugador: ". $jugador->getEdad()."<br><br>";
+
+    $equipo = $jugador->getEquipo();
+    echo "Nombre del equipo: ". $equipo->getNombre();
+    echo "<br>";
+    echo "Número de socios: ". $equipo->getSocios();
+    echo "<br>";
+    echo "Año de fundación: ". $equipo->getFundacion();
+    echo "<br>";
+    echo "Ciudad: ". $equipo->getCiudad();
+
+  }
---
+++ U6/equipo-jugador/src/Entity/Jugador.php
@@ -0,0 +1,162 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jugador
+ *
+ * @ORM\Table(name="jugador", indexes={@ORM\Index(name="jugador_equipo_FK", columns={"equipo"})})
+ * @ORM\Entity
+ */
+class Jugador
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_jugador", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idJugador;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=50, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=100, nullable=false)
+     */
+    private $apellidos;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="edad", type="integer", nullable=true)
+     */
+    private $edad;
+
+    /**
+     * @var \Equipo
+     *
+     * @ORM\ManyToOne(targetEntity="Equipo")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="equipo", referencedColumnName="id_equipo")
+     * })
+     */
+    private $equipo;
+
+
+
+    /**
+     * Get the value of idJugador
+     *
+     * @return  int
+     */ 
+    public function getIdJugador()
+    {
+        return $this->idJugador;
+    }
+
+    /**
+     * Get the value of nombre
+     *
+     * @return  string
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param  string  $nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre(string $nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellidos
+     *
+     * @return  string
+     */ 
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     *
+     * @param  string  $apellidos
+     *
+     * @return  self
+     */ 
+    public function setApellidos(string $apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of edad
+     *
+     * @return  int|null
+     */ 
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @param  int|null  $edad
+     *
+     * @return  self
+     */ 
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of equipo
+     *
+     * @return  \Equipo
+     */ 
+    public function getEquipo()
+    {
+        return $this->equipo;
+    }
+
+    /**
+     * Set the value of equipo
+     *
+     * @param  \Equipo  $equipo
+     *
+     * @return  self
+     */ 
+    public function setEquipo(\Equipo $equipo)
+    {
+        $this->equipo = $equipo;
+
+        return $this;
+    }
+}
